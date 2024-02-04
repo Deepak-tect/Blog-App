f652139c704d39f757325437a6c127a1213233ca
deepak-tect <dddeepak888@gmail.com>
2024-02-04 09:31:13 +0530

    initial setup

---
+++ src/main/java/com/blog/blogingapp/BlogingAppApplication.java
@@ -0,0 +1,14 @@
+package com.blog.blogingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogingAppApplication.class, args);
+		System.out.println("-----********hello********-----------");
+	}
+
+}
